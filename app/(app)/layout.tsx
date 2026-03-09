7c69d8e6d8c8055987e3d14d3ed5817959fb461a
esam191 <esam191@hotmail.com>
2026-03-09 15:27:45 -0400

    feat: initial commit

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
